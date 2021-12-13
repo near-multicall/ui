98900e91ccf118b3f65e1fa7279b8cf92b1e1e6c
lennart Czardybon <lennczar@gmail.com>
2021-12-13 02:54:22 +0100

    removed initContract

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { initContract } from './utils'
 
-window.nearInitPromise = initContract()
-  .then(() => {
-    ReactDOM.render(
-      <App />,
-      document.querySelector('#root')
-    )
-  })
-  .catch(console.error)
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
