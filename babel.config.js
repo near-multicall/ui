52495496332258b50b521d76d70c6c00090f1f7d
lennart Czardybon <lennczar@gmail.com>
2021-12-13 02:32:21 +0100

    yet another attempt to get gh-pages online

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-transform-runtime']
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "esmodules": true
+        }
+      }
+    ],
+    '@babel/preset-react'
+  ],
+  "plugins": [
+    ["transform-runtime", {
+      "regenerator": true
+    }]
+  ]
 }
