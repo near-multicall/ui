34462c4d0fa3589db4b97677c84566600102fd13
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2022-03-16 00:02:04 +0100

    Updates

--- global.65e27967.js
+++ global.65e27967.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41739" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41265" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
