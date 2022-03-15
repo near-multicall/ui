59a0583462083e8034d304727053aa4e1c371c9f
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2022-03-15 23:42:10 +0100

    check if typedArgs cause failure

--- src/pages/dao-page.js
+++ src/pages/dao-page.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import { Dao } from '../components.js';
+import { ArgsAccount, ArgsNumber, ArgsJSON, ArgsObject, ArgsString, ArgsBig, ArgsArray } from '../utils/args';
 
 export default function DaoPage() {
 
-    return(
-        <Dao />
-    );
+    const test = {
+        string: new ArgsString("hello"),
+        account: new ArgsAccount("lennczar.near"),
+        number: new ArgsNumber(123),
+        big: new ArgsBig("123"),
+        object: new ArgsObject(new Object()),
+        array: new ArgsArray(),
+        json: new ArgsJSON("{}")
+    };
+
+    return <div>Potato</div>
 
 }
