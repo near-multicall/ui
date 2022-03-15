63f3f0ffdd8596959a10c8b9073673adaa44b047
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2022-03-15 20:13:57 +0100

    use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import AppPage from './pages/app-page';
 import DaoPage from './pages/dao-page';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-        <Route path="/multicall-UI/" element={<div>Base</div>} />
-        <Route path="/multicall-UI/app" element={<div>App</div>} />
-        <Route path="/multicall-UI/dao" element={<div>Dao</div>} />
+        <Route path="/" element={<div>Base</div>} />
+        <Route path="/app" element={<div>App</div>} />
+        <Route path="/dao" element={<div>Dao</div>} />
       {/* <Route path="/multicall-UI" element={<App />}>
         <Route path="/multicall-UI/" element={<AppPage />} />
         <Route path="/multicall-UI/app" element={<AppPage />} />
         <Route path="/multicall-UI/dao" element={<DaoPage />} />
       </Route> */}
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   document.querySelector('#root')
 );
