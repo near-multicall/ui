d4daa7edc5cfd72b9860814cad269cf5a94cea54
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2022-06-03 23:35:06 +0200

    switch placement of card edit, clone & delete icons

--- src/families/base.js
+++ src/families/base.js
@@ -199,25 +199,6 @@ export default class BaseTask extends Component {
                 className={`task-container ${this.uniqueClassName} ${hasErrors ? "has-errors" : ""} ${isEdited ? "is-edited" : ""}`}
             >
                 <div className="name">
-                    <Tooltip title={<h1 style={{ fontSize: "12px" }}>Delete</h1>} disableInteractive >
-                        <DeleteOutline
-                            className="delete icon"
-                            onClick={() => {
-                                LAYOUT.deleteTask(id);
-                            }}
-                        />
-                    </Tooltip>
-                    <div className="delete-pseudo"></div>
-                    <Tooltip title={<h1 style={{ fontSize: "12px" }}>Clone card</h1>} disableInteractive >
-                        <MoveDown
-                            className="duplicate icon"
-                            onClick={() => {
-                                LAYOUT.duplicateTask(id);
-                            }}
-                        />
-                    </Tooltip>
-                    <div className="duplicate-pseudo"></div>
-                    <h3>{name.toString()}</h3>
                     <Tooltip title={<h1 style={{ fontSize: "12px" }}>Edit</h1>} disableInteractive >
                         <EditOutlined
                             className="edit icon"
@@ -228,6 +209,25 @@ export default class BaseTask extends Component {
                         />
                     </Tooltip>
                     <div className="edit-pseudo"></div>
+                    <Tooltip title={<h1 style={{ fontSize: "12px" }}>Clone card</h1>} disableInteractive >
+                        <MoveDown
+                            className="duplicate icon"
+                            onClick={() => {
+                                LAYOUT.duplicateTask(id);
+                            }}
+                        />
+                    </Tooltip>
+                    <div className="duplicate-pseudo"></div>
+                    <h3>{name.toString()}</h3>
+                    <Tooltip title={<h1 style={{ fontSize: "12px" }}>Delete</h1>} disableInteractive >
+                        <DeleteOutline
+                            className="delete icon"
+                            onClick={() => {
+                                LAYOUT.deleteTask(id);
+                            }}
+                        />
+                    </Tooltip>
+                    <div className="delete-pseudo"></div>
                 </div>
                 <div className="data-container">
                     <p><span>Contract address</span><a className="code" href={addr.toUrl(window.nearConfig.networkId)} target="_blank" rel="noopener noreferrer">{addr.toString()}</a></p>
