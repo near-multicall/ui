bb09963f61ae318cddf43188dc370ccafad19df4
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2022-07-19 23:16:17 +0200

    save local storage keys as constants

--- src/utils/persistent.ts
+++ src/utils/persistent.ts
@@ -1,6 +1,8 @@
 import { initialData } from '../initial-data.js'
 
 export class Persistent {
+    static STORAGE_KEY_ADDRESSES = "multicall_addresses";
+    static STORAGE_KEY_JSON = "multicall_json";
 
     addresses = {
         user: "",
@@ -50,14 +52,14 @@ export class Persistent {
     save() {
         if (window.PAGE !== "app") return;
 
-        localStorage.setItem("multicall_addresses", JSON.stringify(this.addresses));
-        localStorage.setItem("multicall_json", JSON.stringify(window.LAYOUT.toJSON()));
+        localStorage.setItem(Persistent.STORAGE_KEY_ADDRESSES, JSON.stringify(this.addresses));
+        localStorage.setItem(Persistent.STORAGE_KEY_JSON, JSON.stringify(window.LAYOUT.toJSON()));
         // localStorage.setItem("multicall_layout", JSON.stringify(this.layout));
     }
 
     load() {
-        this.setAddresses(JSON.parse(localStorage.getItem("multicall_addresses") ?? "{}"));
-        window.LAYOUT?.fromJSON(JSON.parse(localStorage.getItem("multicall_json") ?? "[]"));
+        this.setAddresses(JSON.parse(localStorage.getItem(Persistent.STORAGE_KEY_ADDRESSES) ?? "{}"));
+        window.LAYOUT?.fromJSON(JSON.parse(localStorage.getItem(Persistent.STORAGE_KEY_JSON) ?? "[]"));
         // this.setLayout(JSON.parse(localStorage.getItem("multicall_layout") ?? "{}"));
     }
 
