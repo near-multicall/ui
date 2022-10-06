d46e948bf8c01d241fdddaf5d011264bacb1532b
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-10-06 22:09:32 +0400

    wip: Display "Start at" job value

--- src/entities/job/ui/jobs-list.tsx
+++ src/entities/job/ui/jobs-list.tsx
@@ -1,6 +1,7 @@
+import { cronToDate } from "../../../shared/lib/converter";
 import { Tile, Scrollable, Table } from "../../../shared/ui/components";
-import { Dependencies } from "../config";
 import { JobDataModel } from "../model/job-data";
+import { Dependencies } from "../config";
 
 interface JobsListProps extends Dependencies {}
 
@@ -15,9 +16,18 @@ export const JobsList = ({ className, contracts }: JobsListProps) => {
                 <Scrollable>
                     <Table
                         displayMode="compact"
-                        header={["Active status", "Croncat hash", "Creator", "Trigger gas", "Run count", "Multicalls"]}
-                        rows={data.map(({ id, job }) => [
+                        header={[
+                            "Active status",
+                            "Start at",
+                            "Croncat hash",
+                            "Creator",
+                            "Trigger gas",
+                            "Run count",
+                            "Multicalls",
+                        ]}
+                        rows={data.map(({ job }) => [
                             job.is_active ? "Active" : "Inactive",
+                            cronToDate(job.cadence).toLocaleString(),
                             job.croncat_hash,
                             job.creator,
                             job.trigger_gas,
