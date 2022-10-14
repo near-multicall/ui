f1767a3108b1c0e8ceea6946507d926bf0bce6c0
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-10-14 18:58:07 +0400

    wip: Override empty object rendering

--- src/shared/ui/components/data-inspector/data-inspector.tsx
+++ src/shared/ui/components/data-inspector/data-inspector.tsx
@@ -1,17 +1,41 @@
 import clsx from "clsx";
 import { MouseEvent, useCallback, useState, type ComponentProps } from "react";
-import { ObjectInspector } from "react-inspector";
+import { ObjectInspector, ObjectLabel, ObjectRootLabel } from "react-inspector";
 
 import { DataInspectorConfig as Config } from "./config";
 import "./data-inspector.scss";
+
+const _DataInspector = "DataInspector";
+
+interface DataInspectorNodeProps {
+    data: unknown;
+    depth: number;
+    expanded: boolean;
+    isNonenumerable: boolean;
+    name: string;
+}
+
+const DataInspectorNode = ({ depth, name, data, isNonenumerable, expanded }: DataInspectorNodeProps) => {
+    const displayData = data && typeof data === "object" && Object.keys(data).length === 0 ? "{}" : data;
+
+    return depth === 0 ? (
+        <ObjectRootLabel
+            data={displayData}
+            {...{ name }}
+        />
+    ) : (
+        <ObjectLabel
+            data={displayData}
+            {...{ isNonenumerable, name }}
+        />
+    );
+};
 
 interface DataInspectorProps extends ComponentProps<typeof ObjectInspector> {
     classes?: { root?: string; body?: string; label?: string };
     expanded?: boolean;
     label?: string;
 }
-
-const _DataInspector = "DataInspector";
 
 export const DataInspector = ({ classes, expanded = false, expandLevel = 1, label, ...props }: DataInspectorProps) => {
     // TODO: Extract custom `<details>` element to separate component.
@@ -43,6 +67,7 @@ export const DataInspector = ({ classes, expanded = false, expandLevel = 1, labe
             <div className={clsx(`${_DataInspector}-body`, classes?.body)}>
                 <ObjectInspector
                     expandLevel={expandLevel < 1 ? 1 : expandLevel}
+                    nodeRenderer={DataInspectorNode}
                     // @ts-ignore Built-in typings seems broken
                     theme={Config.theme}
                     {...props}
