499195a01ae29dbbd7b0d40f81c8b526da331132
lennart Czardybon <lennczar@gmail.com>
2022-10-19 17:24:40 +0200

    added token.ready check to toCall

--- src/families/near/mft-transfer.tsx
+++ src/families/near/mft-transfer.tsx
@@ -120,6 +120,7 @@ export class MftTransfer extends BaseTask<FormData, Props, State> {
 
         if (!arx.big().isValidSync(gas)) throw new CallError("Failed to parse gas input value", this.props.id);
         if (!arx.big().isValidSync(amount)) throw new CallError("Failed to parse amount input value", this.props.id);
+        if (!token.ready) throw new CallError("Lacking token metadata", this.props.id);
 
         return {
             address: addr,
