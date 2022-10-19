eeffa18c2d1ee615cbb5655c35534e76ccecc629
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-10-19 22:56:44 +0400

    wip: Add additional export with more precise naming

--- src/shared/lib/contracts/sputnik-dao.ts
+++ src/shared/lib/contracts/sputnik-dao.ts
@@ -621,5 +621,5 @@ class SputnikDAO {
     }
 }
 
-export { SputnikDAO, SputnikUI, ProposalKindPolicyLabel, ProposalStatus };
+export { SputnikDAO, SputnikDAO as SputnikDAOContract, SputnikUI, ProposalKindPolicyLabel, ProposalStatus };
 export type { FunctionCall, FunctionCallAction, ProposalOutput, ProposalKind, ProposalAction };
