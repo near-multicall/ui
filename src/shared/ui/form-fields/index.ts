9a5a083c5d1ac0d082848519dbe843828c628d14
lennart Czardybon <lennczar@gmail.com>
2022-10-19 19:55:04 +0200

    added info fields

--- src/shared/ui/form-fields/index.ts
+++ src/shared/ui/form-fields/index.ts
@@ -2,3 +2,4 @@ export { TextField } from "./text-field/text-field";
 export { SelectField } from "./select-field/select-field";
 export { UnitField } from "./unit-field/unit-field";
 export { CheckboxField } from "./checkbox-field/checkbox-field";
+export { InfoField } from "./info-field/info-field";
