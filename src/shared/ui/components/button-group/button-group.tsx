1839ce6a5e66d3d333a85f5566bd4b245b462bd7
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-10-20 05:12:57 +0400

    wip: Update & fix button group styles

--- src/shared/ui/components/button-group/button-group.tsx
+++ src/shared/ui/components/button-group/button-group.tsx
@@ -1,12 +1,14 @@
 import clsx from "clsx";
 import { HTMLProps } from "react";
 
-import "./button-group";
+import "./button-group.scss";
 
-export interface ButtonGroupProps extends HTMLProps<HTMLDivElement> {}
+export interface ButtonGroupProps extends HTMLProps<HTMLDivElement> {
+    placement?: "end" | "start";
+}
 
 const _ButtonGroup = "ButtonGroup";
 
-export const ButtonGroup = ({ children, className }: ButtonGroupProps) => (
-    <div className={clsx(_ButtonGroup, className)}>{children}</div>
+export const ButtonGroup = ({ children, className, placement = "end" }: ButtonGroupProps) => (
+    <div className={clsx(_ButtonGroup, `${_ButtonGroup}--${placement}`, className)}>{children}</div>
 );
