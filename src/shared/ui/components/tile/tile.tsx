84e343c2990743d11c6a54f3e2c11c59b2319aee
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-10-21 15:36:17 +0400

    wip: Add footer slot to tile

--- src/shared/ui/components/tile/tile.tsx
+++ src/shared/ui/components/tile/tile.tsx
@@ -8,10 +8,11 @@ import "./tile.scss";
 const _Tile = "Tile";
 
 export interface TileProps extends PropsWithChildren, Omit<HTMLAttributes<HTMLDivElement>, "className"> {
-    classes?: Partial<Record<"root" | "heading" | "content", HTMLAttributes<HTMLDivElement>["className"]>>;
+    classes?: Partial<Record<"root" | "content" | "footer" | "heading", HTMLAttributes<HTMLDivElement>["className"]>>;
     error?: Error | null;
+    footer?: JSX.Element | null;
     heading?: string | null;
-    headingSlotsContent?: { left?: JSX.Element; right?: JSX.Element };
+    headingCorners?: { left?: JSX.Element; right?: JSX.Element };
     loading?: boolean;
     noData?: boolean;
 }
@@ -20,22 +21,23 @@ export const Tile = ({
     children,
     classes,
     error,
+    footer,
     heading,
-    headingSlotsContent,
+    headingCorners,
     loading = false,
     noData = false,
 }: TileProps) => (
     <div className={clsx(_Tile, classes?.root)}>
         {heading && (
             <span className={clsx(`${_Tile}-heading`, classes?.heading)}>
-                {headingSlotsContent?.left && (
-                    <span className={`${_Tile}-heading-slot--left`}>{headingSlotsContent?.left}</span>
+                {headingCorners?.left && (
+                    <span className={`${_Tile}-heading-corner--left`}>{headingCorners?.left}</span>
                 )}
 
                 <h1 className={`${_Tile}-heading-text`}>{heading}</h1>
 
-                {headingSlotsContent?.right && (
-                    <span className={`${_Tile}-heading-slot--right`}>{headingSlotsContent?.right}</span>
+                {headingCorners?.right && (
+                    <span className={`${_Tile}-heading-corner--right`}>{headingCorners?.right}</span>
                 )}
             </span>
         )}
@@ -53,5 +55,7 @@ export const Tile = ({
 
             {!loading && !noData && !error && children}
         </div>
+
+        {footer && <div className={clsx(`${_Tile}-footer`, classes?.footer)}>{footer}</div>}
     </div>
 );
