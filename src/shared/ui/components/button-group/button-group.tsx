173baa83649b3feac9c6a97d7df0a0fa39cee868
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-11-07 15:02:04 +0400

    wip: Change button group default placement

--- src/shared/ui/components/button-group/button-group.tsx
+++ src/shared/ui/components/button-group/button-group.tsx
@@ -4,11 +4,11 @@ import { HTMLProps } from "react";
 import "./button-group.scss";
 
 export interface ButtonGroupProps extends HTMLProps<HTMLDivElement> {
-    placement?: "end" | "start";
+    placement?: "auto" | "end" | "start";
 }
 
 const _ButtonGroup = "ButtonGroup";
 
-export const ButtonGroup = ({ children, className, placement = "end" }: ButtonGroupProps) => (
+export const ButtonGroup = ({ children, className, placement = "auto" }: ButtonGroupProps) => (
     <div className={clsx(_ButtonGroup, `${_ButtonGroup}--${placement}`, className)}>{children}</div>
 );
