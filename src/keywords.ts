588c125ab9b60db14300030422c54d9476842487
lennart Czardybon <lennczar@gmail.com>
2022-11-08 16:36:15 +0100

    fix: dnd when filtering

---
+++ src/keywords.ts
@@ -0,0 +1,24 @@
+export const keywords = {
+    "": {
+        "": ["custom"],
+    },
+    near: {
+        ft_transfer: ["pay"],
+        ft_transfer_call: ["pay"],
+        nft_transfer: [],
+        nft_transfer_call: [],
+        nft_approve: [],
+        nft_revoke: ["revoke_all"],
+        mft_transfer: ["LP"],
+        mft_transfer_call: ["LP"],
+        deposit_and_stake: ["validator"],
+        unstake: ["validator", "unstake_all"],
+        withdraw: ["validator"],
+        storage_withdraw: [],
+        storage_unregister: [],
+        storage_deposit: ["pay"],
+    },
+    multicall: {
+        near_transfer: ["pay"],
+    },
+};
