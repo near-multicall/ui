134d7fb80f641ce36185faf269058b0a3ef68b4c
Rin 🦊🪐😈 Akaia <cvo.akaia@gmail.com>
2022-11-18 21:27:43 +0400

    wip: Create Ref Finance contract adapter class

---
+++ src/shared/lib/contracts/index.ts
@@ -0,0 +1,1 @@
+export { RefFinanceContract } from "./ref-finance";
---
+++ src/shared/lib/contracts/ref-finance.ts
@@ -0,0 +1,7 @@
+class RefFinanceContract {
+    public static readonly tokens = {
+        xREF: { decimals: 18, symbol: "xREF" },
+    };
+}
+
+export { RefFinanceContract };
